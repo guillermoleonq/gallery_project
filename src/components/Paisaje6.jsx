8478f6b3d598ac4ea8c475b4a6f059108c2a5041
guillermoleonq <guillermoleonq@yahoo.com>
2024-09-26 18:07:16 -0500

    update

--- src/components/Paisaje6.jsx
+++ src/components/Paisaje6.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export const Paisaje6 = ( {className} ) => {
   return  (
-    <img src={paisaje6} alt="Flores al atardecer" 
+    <img src={paisaje6} alt="Flores al atardecer en la tarde" 
     className={className} />
   )
 }
